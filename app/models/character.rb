3fd3b32f13cbbe5a0156d91d0ab184e59b2123d4
Justin Daniel <justinddaniel@gmail.com>
2018-01-09 17:23:58 -0500

    setting up character model to mirror traits of associated universe

--- app/models/character.rb
+++ app/models/character.rb
@@ -2,4 +2,10 @@ class Character < ApplicationRecord
 	belongs_to :universe
 	has_many :character_traits
 	has_many :traits, through: :character_traits
+
+	def construct_traits
+		self.universe.traits.each do |trait|
+			self.traits << trait 
+		end
+	end
 end
