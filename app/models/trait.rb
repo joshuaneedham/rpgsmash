4d6991ba798c48dcf6b38d4b3df3b975bb4d5693
Justin Daniel <justinddaniel@gmail.com>
2018-01-16 17:00:27 -0500

    Now have a working class level ActiveRecord Scope method for the Trait model.

--- app/models/trait.rb
+++ app/models/trait.rb
@@ -5,4 +5,15 @@ class Trait < ApplicationRecord
 	validates :name, presence: true
 	validates :description, presence: true
 	validates :datatype, presence: true
+
+	def self.min_and_max_character_traits(trait_id)
+		trait = Trait.find(trait_id)
+		if trait.datatype == "integer" || trait.datatype == "Integer"
+			max = trait.character_traits.order(stat: :desc).limit(1)
+			min = trait.character_traits.order(stat: :asc).limit(1)
+			"The #{trait.name} trait has a current high stat of #{max[0].stat} and a current low stat of #{min[0].stat}."
+		else 
+			"This trait is not an integer datatype and thus does not support this method."
+		end
+	end
 end
