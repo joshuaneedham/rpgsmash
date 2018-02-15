1112935e186b5cb5a649f897c0440114c761c412
Justin Daniel <justinddaniel@gmail.com>
2018-02-15 18:45:35 -0500

    Wired up other character associations. All associations for traits, character_traits, universe now show up on character show in JSON

--- app/serializers/character_serializer.rb
+++ app/serializers/character_serializer.rb
@@ -1,4 +1,6 @@
 class CharacterSerializer < ActiveModel::Serializer
   attributes :id, :name, :biography, :universe_id
   belongs_to :universe
+  has_many :character_traits
+  has_many :traits, through: :character_traits
 end
