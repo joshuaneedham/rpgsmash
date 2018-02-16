a0c0b6b4513226ce10388e7c64ec90cab066bcb0
Justin Daniel <justinddaniel@gmail.com>
2018-02-15 20:42:54 -0500

    Tried troubleshooting the button and click event on the universes show page. No success yet.

--- app/assets/javascripts/rpg.js
+++ app/assets/javascripts/rpg.js
@@ -1,18 +1,23 @@
-function charInfo () {
-      var id = $(this).data("id");
-    $.get("/characters/" + id + ".json", function(data) {
-      var character = data;
 
-      var biographyText = "<p>" + character["biography"] + "</p>"
-      $("#character-" + id).html(biographyText);
-      var characterTraits = character["character_traits"];
-      var charTraitList = "";
-      characterTraits.forEach(function(charTrait) {
-        charTraitList += '<li class="js-chartrait" data-id="' + charTrait["id"] + '">' + charTrait["trait_id"] + ' - ' 
-        + charTrait["stat"] + '</li>';
-      });
-      $("#character-" + id + "-characterTraits").html(charTraitList);
-    });
+function charInfo () {
+  alert("I have been clicked!");
 }
+
+//function charInfo () {
+//      var id = $(this).data("id");
+//    $.get("/characters/" + id + ".json", function(data) {
+//      var character = data;
+//
+//      var biographyText = "<p>" + character["biography"] + "</p>"
+//      $("#character-" + id).html(biographyText);
+//      var characterTraits = character["character_traits"];
+//      var charTraitList = "";
+//      characterTraits.forEach(function(charTrait) {
+//        charTraitList += '<li class="js-chartrait" data-id="' + charTrait["id"] + '">' + charTrait["trait_id"] + ' - ' 
+//        + charTrait["stat"] + '</li>';
+//      });
+//      $("#character-" + id + "-characterTraits").html(charTraitList);
+//    });
+//}
 
 
