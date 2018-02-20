dc238bfdd0d0738cf2aa779515ba7ff392014cf0
Justin Daniel <justinddaniel@gmail.com>
2018-02-20 17:17:55 -0500

    eliminated comments no longer needed

--- app/assets/javascripts/rpg.js
+++ app/assets/javascripts/rpg.js
@@ -25,22 +25,3 @@ function charInfo () {
     $("#character-" + charButtonID + "-stats").html(charTraitList);
   })
 } 
-
-//function charInfo () {
-//      var id = $(this).data("id");
-//    $.get("/characters/" + id + ".json", function(data) {
-//      var character = data;
-//
-//      var biographyText = "<p>" + character["biography"] + "</p>"
-//      $("#character-" + id).html(biographyText);
-//      var characterTraits = character["character_traits"];
-//      var charTraitList = "";
-//      characterTraits.forEach(function(charTrait) {
-//        charTraitList += '<li class="js-chartrait" data-id="' + charTrait["id"] + '">' + charTrait["trait_id"] + ' - ' 
-//        + charTrait["stat"] + '</li>';
-//      });
-//      $("#character-" + id + "-characterTraits").html(charTraitList);
-//    });
-//}
-
-
